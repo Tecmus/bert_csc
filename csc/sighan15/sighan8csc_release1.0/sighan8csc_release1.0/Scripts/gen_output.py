b72541f6081c61f161b2f934118aa723e8b8ad1b
Ubuntu <azureuser@micro.4jfcojxnrshu3jvbrsixo5sewh.bx.internal.cloudapp.net>
2021-04-12 06:25:13 +0000

    fix gen_output

--- csc/sighan15/sighan8csc_release1.0/sighan8csc_release1.0/Scripts/gen_output.py
+++ csc/sighan15/sighan8csc_release1.0/sighan8csc_release1.0/Scripts/gen_output.py
@@ -1,18 +1,18 @@
 #coding:utf-8
 
 def load_file(sighan_test, pred_file):
-    raw_lines = [for x in open(sighan)]
-    pred_lines = [for x in open(pred_file))]
+    raw_lines = [x for x in open(sighan)]
+    pred_lines = [x for x in open(pred_file)]
 
     assert len(raw_lines) == len(pred_lines)
     return raw_lines, pred_lines
 
 def gen_test_format(raw_lines, pred_lines):
     for origin, pred in zip(raw, pred_lines):
-    pid, origin_input=origin.split('\t')
-    pid=pid[1:-1]
-    format_out=build_format(pid,mpred)
-    print(format_out)
+        pid, origin_input=origin.split('\t')
+        pid=pid[1:-1]
+        format_out=build_format(pid,mpred)
+        print(format_out)
     
 def build_format(pid,pred):
     pred_input, pred_out=pred.split('\t')
