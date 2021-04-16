1063865eff9ac1ea2fdef527d8565ad412a4cd13
Your Name <you@example.com>
2021-04-16 09:02:37 +0000

    add predictor

--- csc/csc_pred.sh
+++ csc/csc_pred.sh
@@ -6,18 +6,8 @@
 # export BERT_BASE_DIR=/kaggle/working/bert_csc/csc/model/chinese_L-12_H-768_A-12
 export BERT_BASE_DIR=/root/model/chinese_L-12_H-768_A-12
 
-PYTHONIOENCODING=utf-8 python run_pretraining.py \
-  --input_file=./record_data/ocr_test_no_shuf.tfrecord \
-  --output_dir=/root/output_2k \
-  --do_train=True \
+PYTHONIOENCODING=utf-8 python csc_predictor.py \
+  --model_dir=./save_model \
   --vocab_file=$BERT_BASE_DIR/vocab.txt \
   --do_lower_case=True \
-  --bert_config_file=$BERT_BASE_DIR/bert_config.json \
-  --init_checkpoint=/root/output_2k/model.ckpt-10000 \
-  --train_batch_size=20 \
-  --max_seq_length=128 \
-  --max_predictions_per_seq=20 \
-  --num_train_steps=100 \
-  --num_warmup_steps=100 \
-  --learning_rate=2e-5 \
-  --top_k=1
+  --max_seq_length=128
--- csc/csc_predictor.py
+++ csc/csc_predictor.py
@@ -1,49 +1,99 @@
 #coding:utf-8
 from tensorflow.contrib import predictor
+import tensorflow as tf
+import collections
+import tokenization
+flags = tf.app.flags
 
+FLAGS = flags.FLAGS
 
+## Required parameters
+flags.DEFINE_string("vocab_file", None,
+                    "The vocabulary file that the BERT model was trained on.")
+flags.DEFINE_string(
+    "model_dir", None,
+    "model path")
+flags.DEFINE_bool(
+    "do_lower_case", True,
+    "Whether to lower case the input text. Should be True for uncased "
+    "models and False for cased models.")
+flags.DEFINE_integer(
+    "max_seq_length", 128,
+    "The maximum total input sequence length after WordPiece tokenization. "
+    "Sequences longer than this will be truncated, and sequences shorter "
+    "than this will be padded. Must match data generation.")
 def create_input(input_ids,input_mask,segment_ids):
-    def create_int_feature(values):
-        feature = tf.train.Feature(int64_list=tf.train.Int64List(value=list(values)))
-        return feature
     features = collections.OrderedDict()
-    features["input_ids"] = create_int_feature(input_ids)
-    features["input_mask"] = create_int_feature(input_mask)
-    features["segment_ids"] = create_int_feature(segment_ids)
-    # tf_example = tf.train.Example(features=tf.train.Features(feature=features))
+    features["input_ids"] = input_ids
+    features["input_mask"] = input_mask
+    features["segment_ids"] = segment_ids
     
     return features
 
-# def _serving_input_receiver_fn():
- 
-#     receiver_tensors = {'examples': serialized_tf_example}
-#     features=create_input()
-#     return tf.estimator.export.ServingInputReceiver(features, receiver_tensors)
+def encode_to_feed_dict(query,tokenizer):
+    query = tokenization.convert_to_unicode(query)
+    input_tokens = tokenizer.tokenize(query)
+    input_tokens=['[CLS]']+input_tokens+['[SEP]']+['[SEP]']
+    assert len(input_tokens) <= FLAGS.max_seq_length
+    input_ids=tokenizer.convert_tokens_to_ids(input_tokens)
+    input_mask = [1] * len(input_ids)
+    segment_ids = [0] * len(input_ids)
+    segment_ids[-1]=1
 
-# def _serving_input_receiver_fn():
-#     """Serving input_fn that builds features from placeholders
-#     Returns
-#     -------
-#     tf.estimator.export.ServingInputReceiver
-#     """
-#     top_k_res = tf.placeholder(dtype=tf.string, shape=[None, None], name='top_k_res')
-#     input_ids = tf.placeholder(dtype=tf.int32, shape=[None,None], name='input_ids')
+    while len(input_ids) < FLAGS.max_seq_length:
+        input_ids.append(0)
+        input_mask.append(0)
+        segment_ids.append(0)
+            
+    feed_dict=create_input([input_ids],[input_mask],[segment_ids])
+    
+    return feed_dict
 
-#     input_ids = tf.placeholder(dtype=tf.int64, shape=[None, None], name='input_ids')
-#     input_mask = tf.placeholder(dtype=tf.int64, shape=[None,None], name='input_mask')
-#     segment_ids = tf.placeholder(dtype=tf.int64, shape=[None,None], name='segment_ids')
+def decode_to_query(ids,tokenizer):
+    tokens=tokenizer.convert_ids_to_tokens(ids)
+    
+    pass
+def process_output(item,tokenizer):
+#   for item in res:
 
-#     receiver_tensors = {'top_k_res': top_k_res, 'input': input_ids}
-#     features = {'input_ids': input_ids, 'input_mask': input_mask,'segment_ids':segment_ids}
+    seq_topk = item['top_k_res']
+    input_ids = item['input']
+    
+    input_ids=input_ids.tolist()[0]
+    seq_topk=seq_topk.tolist()[0]
 
-#     return tf.estimator.export.ServingInputReceiver(features, receiver_tensors)
+    input_tokens= [token 
+            for token in  tokenizer.convert_ids_to_tokens(input_ids)
+            if token != '[PAD]']
+    len_input= len(input_tokens)
+    input_seq=input_tokens[1:len_input-2]
+    output_seq=[]
+    #debug
+    # print(input_seq)
+    for input_token,seq in zip(input_seq, seq_topk[1:len_input-1]):
+        out_candi_seq=tokenizer.convert_ids_to_tokens(seq)
+        # out_candi_seq=[vocab_words[token_idx] for token_idx in seq]
+        # print(out_candi_seq)
+        candi_set = set(out_candi_seq)
+        if input_token in candi_set:
+            output_seq.append(input_token)
+        else:  
+            output_seq.append(out_candi_seq[0])
+      
+    return ''.join(input_seq),''.join(output_seq)
 
-# estimator.export_savedmodel(export_dir, _serving_input_receiver_fn)
+def main():
+    model_dir='./save_model/1618549779'
+    predict_fn = predictor.from_saved_model(model_dir)
+    query='今天的天晴不错'
+    query='你小起来真好看'
+    tokenizer = tokenization.FullTokenizer(
+    vocab_file=FLAGS.vocab_file, do_lower_case=FLAGS.do_lower_case)
+    
+    feed_dict=encode_to_feed_dict(query,tokenizer)
+    pred = predict_fn(feed_dict)
+    res=process_output(pred,tokenizer)
+    print(res)
+if __name__ == "__main__":
 
-model_dir='./save_model/1618512160'
-predict_fn = predictor.from_saved_model(model_dir)
-
-input_ids=[1]*128
-feed_dict=create_input(input_ids,input_ids,input_ids)
-pred = predict_fn(feed_dict)['output']
-print(pred)
+    main()
